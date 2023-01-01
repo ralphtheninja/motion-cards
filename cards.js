653b80fc568f1ab5119fc6908c5017322fbd6e15
Lars-Magnus Skog <ralphtheninja@riseup.net>
2023-01-01 15:56:31 +0100

    Introduce some real cards

--- cards.js
+++ cards.js
@@ -1,35 +1,110 @@
 const allCards = [
   {
-    title: 'card1',
+    title: '2 undantag i LAS för mindre företag',
     color: 'blue',
     id: 1
   },
   {
-    title: 'card2',
-    color: 'blue',
+    title: 'Sjukpenninggrundande inkomst',
+    color: 'red',
     id: 2
   },
   {
-    title: 'card3',
-    color: 'blue',
+    title: 'Statsbudget 2003',
+    color: 'red',
     id: 3
   },
   {
-    title: 'card4',
+    title: 'Finansiering av boendet',
     color: 'red',
     id: 4
   },
   {
-    title: 'card5',
+    title: 'Statsbudget 2004',
     color: 'red',
     id: 5
+  },
+  {
+    title: 'Karensdagen',
+    color: 'red',
+    id: 6
+  },
+  {
+    title: 'Flexibla sjukskrivningsnivåer i sjukförsäkringen',
+    color: 'red',
+    id: 7
+  },
+  {
+    title: 'Statsbudget 2005',
+    color: 'red',
+    id: 8
+  },
+  {
+    title: 'Samhällsekonomisk konsekvensanalys vid nedskärning',
+    color: 'red',
+    id: 9
+  },
+  {
+    title: 'Statsbudget 2006',
+    color: 'red',
+    id: 10
+  },
+  {
+    title: 'Första Hjälpen',
+    color: 'red',
+    id: 11
+  },
+  {
+    title: 'Jämställdhet',
+    color: 'blue',
+    id: 12
+  },
+  {
+    title: 'Statsbudget 2007',
+    color: 'blue',
+    id: 13
+  },
+  {
+    title: 'Ändring i inkomstskattelagen',
+    color: 'blue',
+    id: 14
+  },
+  {
+    title: 'En a-kassa för arbete',
+    color: 'blue',
+    id: 15
+  },
+  {
+    title: 'Höjd sjukersättning',
+    color: 'red',
+    id: 16
+  },
+  {
+    title: 'Friskvård under arbetstid',
+    color: 'red',
+    id: 17
+  },
+  {
+    title: 'Multinationella företag och fackliga rättigheter',
+    color: 'red',
+    id: 18
+  },
+  {
+    title: 'Ytterligare reformer inom arbetsmarknadspolitiken',
+    color: 'blue',
+    id: 19
+  },
+  {
+    title: 'Auktorisering av bemanningsföretag',
+    color: 'red',
+    id: 20
   }
 ]
 
 const slides = [
   {
     name: 'slide1',
-    cards: [5, 1, 4]
+    cards: [1,4,7,9,12,14,15,17,19,20]
   }
 ]
 
